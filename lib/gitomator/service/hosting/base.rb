ad3612f088bade1271a7c749351518b04201b55f
Joey Freund <joeyfreund@gmail.com>
2016-02-28 19:17:00 -0500

    Adding unimplemented methods to the base hosting-service. Essentially, this is the interface of the hosting service provider.

--- lib/gitomator/service/hosting/base.rb
+++ lib/gitomator/service/hosting/base.rb
@@ -9,6 +9,129 @@ module Gitomator
           self.send(gitom.action, *(gitom.replay_args))
         end
 
+        # ----------------------- CRUD operations on repos ---------------------
+
+        def create_repo(name, opts)
+          raise "Unsupported"
+        end
+
+        def read_repo(name)
+          raise "Unsupported"
+        end
+
+        def update_repo(name, opts)
+          raise "Unsupported"
+        end
+
+        def delete_repo(name)
+          raise "Unsupported"
+        end
+
+        # ----------------------------------------------------------------------
+
+
+        # ----------------------- CRUD operations on users ---------------------
+
+        def create_user(name, opts)
+          raise "Unsupported"
+        end
+
+        def read_user(name)
+          raise "Unsupported"
+        end
+
+        def update_user(name, opts)
+          raise "Unsupported"
+        end
+
+        def delete_user(name)
+          raise "Unsupported"
+        end
+
+        # ----------------------------------------------------------------------
+
+        # ----------------------- CRUD operations on groups --------------------
+
+        def create_group(name, opts)
+          raise "Unsupported"
+        end
+
+        def read_group(name)
+          raise "Unsupported"
+        end
+
+        def update_group(name, opts)
+          raise "Unsupported"
+        end
+
+        def delete_group(name)
+          raise "Unsupported"
+        end
+
+        # ----------------------------------------------------------------------
+
+
+        # ------------ CRUD operations on group_memberships --------------------
+
+        def create_group_membership(group, user, opts)
+          raise "Unsupported"
+        end
+
+        def read_group_membership(group, user)
+          raise "Unsupported"
+        end
+
+        def update_group_membership(group, user, opts)
+          raise "Unsupported"
+        end
+
+        def delete_group_membership(group, user)
+          raise "Unsupported"
+        end
+
+        # ----------------------------------------------------------------------
+
+        # --------------- CRUD operations on permissions -----------------------
+
+        def create_permission(user_or_group, repo, opts)
+          raise "Unsupported"
+        end
+
+        def read_permission(user_or_group, repo)
+          raise "Unsupported"
+        end
+
+        def update_permission(user_or_group, repo, opts)
+          raise "Unsupported"
+        end
+
+        def delete_permission(user_or_group, repo)
+          raise "Unsupported"
+        end
+
+        # ----------------------------------------------------------------------
+
+
+        # ------------------- CRUD operations on pull-requests -----------------
+
+        def create_pull_reuqest(src, dst, opts)
+          raise "Unsupported"
+        end
+
+        def read_pull_reuqest(src, dst)
+          raise "Unsupported"
+        end
+
+        def update_pull_reuqest(src, dst, opts)
+          raise "Unsupported"
+        end
+
+        def delete_pull_reuqest(src, dst)
+          raise "Unsupported"
+        end
+
+        # ----------------------------------------------------------------------
+
       end
     end
   end
