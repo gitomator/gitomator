3d2e16a3b765a216bbc42eeedd260616d10e036a
Joey Freund <joeyfreund@gmail.com>
2016-02-29 01:38:53 -0500

    Adding the interface for the Git service

---
+++ lib/gitomator/service/git/base.rb
@@ -0,0 +1,35 @@
+require 'gitomator/gitom/hosting/repo'
+
+module Gitomator
+  module Service
+    module Git
+      class Base  # Essentially, the interface for a generic Git service
+
+        def clone(repo_url, local_repo_root, opts)
+          raise "Unsupported"
+        end
+
+        def add(local_repo_root, path, opts)
+          raise "Unsupported"
+        end
+
+        def commit(local_repo_root, message, opts)
+          raise "Unsupported"
+        end
+
+        def checkout(local_repo_root, branch, opts)
+          raise "Unsupported"
+        end
+
+        def set_remote(local_repo_root, remote, url, opts)
+          raise "Unsupported"
+        end
+
+        def push(local_repo_root, remote, opts)
+          raise "Unsupported"
+        end
+
+      end
+    end
+  end
+end
