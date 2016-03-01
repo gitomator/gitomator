db5eab5877400033b35d366c25336638a82f39da
Joey Freund <joeyfreund@gmail.com>
2016-03-01 01:53:05 -0500

    Trivial

--- spec/lib/hosting_provider_interface_spec.rb
+++ spec/lib/hosting_provider_interface_spec.rb
@@ -20,9 +20,8 @@ describe Gitomator::Service::Hosting::Service do
 
 
   it "read_repo should return a repo model-object" do
-    repo = "test-repo-#{(Time.now.to_f * 1000).to_i}"
-    @hosting.create_repo(repo, {})
-    expect(@hosting.read_repo(repo)).to be_a_kind_of(Gitomator::Model::Hosting::Repo)
+    repo = @hosting.create_repo("test-repo-#{(Time.now.to_f * 1000).to_i}", {})
+    expect(@hosting.read_repo(repo.name)).to be_a_kind_of(Gitomator::Model::Hosting::Repo)
   end
 
   it "read_repo should return nil when repo doesn't exist" do
