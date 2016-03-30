084dae2e1d7cfaefc7f6980bdcb6a4d444015f5e
Joey Freund <joeyfreund@gmail.com>
2016-03-30 19:49:03 -0400

    Adding the Gitomator::Util::Repo::NameResolver class

---
+++ lib/gitomator/util/repo/name_resolver.rb
@@ -0,0 +1,57 @@
+module Gitomator
+  module Util
+    module Repo
+
+      #
+      # Convenience class dealing with repo names of the format "namespace/name".
+      #
+      class NameResolver
+
+        attr_reader :default_namespace
+
+        def initialize(default_namespace=nil)
+          @default_namespace = default_namespace
+        end
+
+
+        def full_name(repo_name)
+          tokenize(repo_name).join("/")
+        end
+
+        def namespace_only(repo_name)
+          tokenize(repo_name).first
+        end
+
+        def name_only(repo_name)
+          tokenize(repo_name).last
+        end
+
+        #
+        # Return an array with two strings, the namespace and name of the given
+        # repo. If the given repo is missing a namespace, self's default
+        # namespace will be used (if there isn't a default namespace, an error
+        # will be raised).
+        #
+        # @param name [String] - The name of the repo in the format "namespace/name".
+        #
+        def tokenize(name)
+          split_name = name.split "/"
+          case split_name.length
+          when 1
+            if @default_namespace.nil?
+              raise "Cannot tokenize repo name, '#{name}' - Missing namespace."
+            else
+              return [@default_namespace, name]
+            end
+          when 2
+            return split_name
+          else
+            raise "Invalid repo name, '#{name}'."
+          end
+        end
+
+
+      end
+    end
+  end
+end
