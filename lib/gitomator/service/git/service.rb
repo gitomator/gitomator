e5a5c51d847a4d0380b550eccce42443f654a507
Joey Freund <joeyfreund@gmail.com>
2016-04-01 01:06:07 -0400

    Trivial

--- lib/gitomator/service/git/service.rb
+++ lib/gitomator/service/git/service.rb
@@ -12,31 +12,31 @@ module Gitomator
 
         # ----------------------------------------------------------------------
 
-        def clone(repo_url, local_repo_root, opts)
+        def clone(repo_url, local_repo_root, opts={})
           _delegate(__callee__, repo_url, local_repo_root, opts)
         end
 
-        def init(local_repo_root, opts)
+        def init(local_repo_root, opts={})
           _delegate(__callee__, local_repo_root, opts)
         end
 
-        def add(local_repo_root, path, opts)
+        def add(local_repo_root, path, opts={})
           _delegate(__callee__, local_repo_root, path, opts)
         end
 
-        def commit(local_repo_root, message, opts)
+        def commit(local_repo_root, message, opts={})
           _delegate(__callee__, local_repo_root, message, opts)
         end
 
-        def checkout(local_repo_root, branch, opts)
+        def checkout(local_repo_root, branch, opts={})
           _delegate(__callee__, local_repo_root, branch, opts)
         end
 
-        def set_remote(local_repo_root, remote, url, opts)
+        def set_remote(local_repo_root, remote, url, opts={})
           _delegate(__callee__, local_repo_root, remote, url, opts)
         end
 
-        def push(local_repo_root, remote, opts)
+        def push(local_repo_root, remote, opts={})
           _delegate(__callee__, local_repo_root, remote, opts)
         end
 
