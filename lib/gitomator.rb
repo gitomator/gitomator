fdf90eb7742580dec3d6e0dcfd16f62c051c829a
Joey Freund <joeyfreund@gmail.com>
2016-04-09 11:29:13 -0400

    Added a utility method to load ERB/YAML files

--- lib/gitomator.rb
+++ lib/gitomator.rb
@@ -1,5 +1,27 @@
 require "gitomator/version"
 
 module Gitomator
-  # Your code goes here...
+
+  module Util
+
+    #
+    # Given a config file (path to file, or an object that responds to :read),
+    # do that ERB+YAML thing, and return a Hash.
+    #
+    # @param config [String/File]
+    # @return Hash
+    #
+    def self.load_config(config)
+      require 'erb'
+      require 'yaml'
+
+      if config.respond_to? :read
+        YAML::load(ERB.new(config.read).result)
+      else
+        YAML::load(ERB.new(File.read(config)).result)
+      end
+    end
+
+  end
+
 end
