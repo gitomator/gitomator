df966cd479a24a27929290621e140e1742119a75
Joey Freund <joeyfreund@gmail.com>
2016-04-12 16:29:44 -0400

    Adding the :update_existing option to the CreateRepos task (defaults to false).

--- lib/gitomator/task/create_repos.rb
+++ lib/gitomator/task/create_repos.rb
@@ -12,6 +12,7 @@ module Gitomator
       # @param opts [Hash<Symbol,Object>]
       # @option opts [Hash]    :create_opts - Options Hash that will be passed to the hosting service when creating the repos.
       # @option opts [String]  :source_repo - The name of a repo that will be the "starting point" of all the created repos.
+      # @option opts [Boolean] :update_existing - Update existing repos, by pushing latest commit(s) from the source_repo.
       #
       def initialize(context, repos, opts={})
         super(context)
@@ -33,30 +34,27 @@ module Gitomator
 
         @repos.each_with_index do |repo_name, index|
           begin
-            repo = create_repo_if_missing(repo_name, index)
-            if(source_repo_local_root)
+            logger.info "#{repo_name} (#{index + 1} out of #{@repos.length})"
+            repo = hosting.read_repo(repo_name)
+
+            if repo.nil?
+              logger.info "Creating repo #{repo_name}"
+              repo = hosting.create_repo(repo_name, @opts[:create_opts] || {})
+              if source_repo_local_root
+                update_repo(repo, source_repo_local_root)
+              end
+            elsif source_repo_local_root && @opts[:update_existing]
               update_repo(repo, source_repo_local_root)
+            else
+              logger.debug("Skipping #{repo_name}, repo already exists.")
             end
+
           rescue => e
             on_error(repo_name, index, e)
           end
         end
       end
 
-
-      def create_repo_if_missing(repo_name, index)
-        logger.info "#{repo_name} (#{index + 1} out of #{@repos.length})"
-
-        repo = hosting.read_repo(repo_name)
-        if (repo.nil?)
-          logger.info "Creating repo #{repo_name}"
-          repo = hosting.create_repo(repo_name, @opts[:create_opts] || {})
-        else
-          logger.debug "Repo #{repo_name} already exists."
-        end
-
-        return repo
-      end
 
 
       def update_repo(repo, source_repo_local_root)
